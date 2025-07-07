5ac823d1eb6d30d8f3aefb62464ef6c83adf5a14
Ritalin <kazu.ktz.tamura@gmail.com>
2025-07-08 00:15:04 +0900

    Update manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/sqlite-parser-playground/',
+    base: '/parser-playground/',
     build: {
         target: 'esnext',
     },
